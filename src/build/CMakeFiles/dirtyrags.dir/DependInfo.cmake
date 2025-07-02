93d75ddcc1931cfcf50430f67e5620122bf422d3
Corey-Burdick <coreyburdickwork@gmail.com>
2025-07-01 20:57:29 -0400

    Added CMake files

---
+++ src/build/CMakeFiles/dirtyrags.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/corey/Code/src/arrsortlib.cpp" "CMakeFiles/dirtyrags.dir/arrsortlib.cpp.o" "gcc" "CMakeFiles/dirtyrags.dir/arrsortlib.cpp.o.d"
+  "/home/corey/Code/src/main.cpp" "CMakeFiles/dirtyrags.dir/main.cpp.o" "gcc" "CMakeFiles/dirtyrags.dir/main.cpp.o.d"
+  "" "dirtyrags" "gcc" "CMakeFiles/dirtyrags.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/CMakeFiles/dirtyrags.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dirtyrags.
+# This may be replaced when dependencies are built.
